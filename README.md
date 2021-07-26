d69252cb84d525eafcf2a09ad9b90127d1d68448
Stylix58 <lateman-jpeg@outlook.fr>
2021-07-26 16:04:29 +0200

    first real draft

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Flative
 A native client for your Flarum-powered communities
+
+## Setup
+```sh
+# setup dependancies
+pip install -r requirements.txt
+# launch the app
+python3 main.py
+```
+
+## License
+This repository is licensed under the [MIT license](LICENSE).
--- main.py
+++ main.py
@@ -3,24 +3,31 @@ from guizero import *
 app = App(title="Flative")
 user = FlarumUser(forum_url="https://discuss.flarum.org")
 
-# layouts
-discussions = ListBox(app, items=[d.title for d in user.all_discussions(
-)], height="fill", width="fill", align="left")
+
+def changeDiscussion(title):
+    d = user.get_discussion_by_id(
+        discussionsIdsCache[discussions.items.index(title)])
+    fp = d.get_posts()[0]
+    discussionText.value = fp.contentHtml
 
 
 def reloadDiscussions():
     discussions.clear()
-    for discussion in user.all_discussions():
-        discussions.append(discussion.title)
+    discussionsIdsCache.clear()
+    for d in user.all_discussions():
+        discussions.append(d.title)
+        discussionsIdsCache.append(d.id)
+
+
+discussions = ListBox(app, items=[d.title for d in user.all_discussions(
+)], height="fill", width="fill", align="left", scrollbar=True, command=changeDiscussion)
+discussionsIdsCache = [d.id for d in user.all_discussions()]
+
+discussionText = TextBox(app, width="fill", height="fill", align="right", scrollbar=True, multiline=True, enabled=False)
 
 
 menubar = MenuBar(app,
                   toplevel=["File"], options=[
-                      [ ["Reload", reloadDiscussions] ]])
-
-rightBox = Box(app, height="fill", width="fill", align="right")
-
-# other
-Text(rightBox, text="Test")
+                      [["Reload", reloadDiscussions]]])
 
 app.display()
